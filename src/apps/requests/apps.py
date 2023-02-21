fc5d3417ebb9bb7648330707d8bdd05eb10be931
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-02-21 16:26:40 -0500

    Apps.py fixed

--- src/apps/requests/apps.py
+++ src/apps/requests/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class RequestsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.requests'
