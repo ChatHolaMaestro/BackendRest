4d7f5ef84b9b2a05d97c061c1e291bc0551f8be1
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-01 10:55:17 -0500

    School and school manager viewset, and router done

--- src/apps/schools/school_api/school_views/schoolViewSet.py
+++ src/apps/schools/school_api/school_views/schoolViewSet.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from apps.shared.shared_api.shared_views.GenericModelViewSets import GenericModelViewSet
+from apps.schools.school_api.school_serializers.schoolSerializers import SchoolSerializer, SchoolManagerSerializer
 
-# Create your views here.
+class SchoolViewset(GenericModelViewSet):
+    '''
+    Generic Viewset for School
+    '''
+    serializer_class = SchoolSerializer
+
+class SchoolManagerViewset(GenericModelViewSet):
+    '''
+    Generic Viewset for School Manager
+    '''
+    serializer_class = SchoolManagerSerializer
