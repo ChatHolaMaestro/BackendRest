0c1bde09c5865b365d21ebc3f4835a0cc3e59e9e
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-01 13:13:05 -0500

    Request model created

---
+++ src/apps/requests/request_models/requestChoices.py
@@ -0,0 +1,7 @@
+#Request Status Choices
+REQUEST_STATUS_CHOICES = (
+    ('PENDIENTE', 'Pendiente'),
+    ('COMPLETADO', 'Completado'),
+    ('CONTACTADO', 'Contactado'),
+    ('CANCELADO', 'Cancelado'),
+)
