a57c03cc460b98d2b09cd2a54d1721701354af5d
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-01 10:41:09 -0500

    School model and school manager done

--- src/apps/schools/school_models/schoolModels.py
+++ src/apps/schools/school_models/schoolModels.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.shared.shared_models.GenericModels import SharedModelHistorical
 
-# Create your models here.
+class School(SharedModelHistorical):
+    '''
+    Model which represents a school
+    '''
+    name = models.CharField("Nombre", max_length=100, unique=True)
+    address = models.CharField("Dirección", max_length=150, blank = False)
+    has_morning_hours = models.BooleanField("Jornada de mañana", default=True, blank = False)
+    has_afternoon_hours = models.BooleanField("Jornada de tarde", default=False, blank = False)
+    
+class SchoolManager(SharedModelHistorical):
+    '''
+    Model which represents a school manager
+    '''
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    #TODO: Add user field
