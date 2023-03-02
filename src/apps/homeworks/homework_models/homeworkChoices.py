62e3aadb7446d69ba272e5e5660b7fc3f665735c
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-02 11:34:51 -0500

    Homework models done

---
+++ src/apps/homeworks/homework_models/homeworkChoices.py
@@ -0,0 +1,6 @@
+#HomeWork Status Choices
+HOMEWORK_STATUS_CHOICES = (
+    ('COMPLETADO', 'Completado'),
+    ('PENDIENTE', 'Pendiente'),
+    ('SIN RESPUESTA', 'Sin Respuesta'),
+)
--- src/apps/homeworks/homework_models/homeworkModels.py
+++ src/apps/homeworks/homework_models/homeworkModels.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.shared.shared_models.GenericModels import SharedModelHistorical
+from apps.homeworks.homework_models.homeworkChoices import HOMEWORK_STATUS_CHOICES
+from apps.requests.request_models.requestModels import Request
 
-# Create your models here.
+class Homework(SharedModelHistorical):
+    '''
+    Model which represents a homework
+    '''
+    status = models.CharField("Estado de la Tarea", max_length=20, choices=HOMEWORK_STATUS_CHOICES, default='PENDIENTE', blank=False, null=False)
+    topic = models.CharField("Tema de la Tarea", max_length=100)
+    details = models.TextField("Detalles de la Tarea", max_length=500)
+    time_spent = models.IntegerField("Tiempo invertido", default=0, blank=False, null=False)
+    scheduled_date = models.DateField("Fecha programada", blank=False, null=False)
+    # Foreign Keys
+    request = models.ForeignKey(Request, on_delete=models.CASCADE, related_name='homeworks')
+    
+    
