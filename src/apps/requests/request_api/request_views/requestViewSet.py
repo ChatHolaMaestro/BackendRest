e1b16467bf1a6374be303099a1036b3c37c45275
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-07 12:04:23 -0500

    Search request by student id feature done

--- src/apps/requests/request_api/request_views/requestViewSet.py
+++ src/apps/requests/request_api/request_views/requestViewSet.py
@@ -13,6 +13,7 @@ class RequestViewSet(GenericModelViewSet):
         - GET(id): get a request by id
         - PUT(id): update a request by id
         - /search_teacher: GET(Teacher id): Get all requests by teacher id
+        - /search_student: GET(Student id): Get all requests by student id
     '''
     serializer_class = RequestViewSerializer
     serializerCreation = RequestCreationSerializer
@@ -26,6 +27,19 @@ class RequestViewSet(GenericModelViewSet):
         teacher = request.query_params.get('teacher')
         if teacher:
             queryset = self.get_queryset().filter(teacher__id=teacher)
+            serializer = self.get_serializer(queryset, many=True)
+            if serializer.data:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'detail': 'No request found'}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'])
+    def search_student(self, request):
+        '''
+        Search request by student
+        '''
+        student = request.query_params.get('student')
+        if student:
+            queryset = self.get_queryset().filter(student__id=student)
             serializer = self.get_serializer(queryset, many=True)
             if serializer.data:
                 return Response(serializer.data, status=status.HTTP_200_OK)
