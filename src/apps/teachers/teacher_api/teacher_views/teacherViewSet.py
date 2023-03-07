bb9fc1ea5c92ed75bc10d3c5b90d0fa23bef99d8
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-07 11:41:15 -0500

    Search Teacher by subjects done

--- src/apps/teachers/teacher_api/teacher_views/teacherViewSet.py
+++ src/apps/teachers/teacher_api/teacher_views/teacherViewSet.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from apps.shared.shared_api.shared_views.GenericModelViewSets import GenericModelViewSet
 from apps.teachers.teacher_api.teacher_serializers.teacherSerializers import TeacherViewSerializer, TeacherCreationSerializer
 from apps.teachers.teacher_api.teacher_serializers.scheduleSerializer import ScheduleViewSerializer, ScheduleCreationSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 class TeacherViewSet(GenericModelViewSet):
     '''
@@ -11,10 +14,25 @@ class TeacherViewSet(GenericModelViewSet):
         - GET(id): get a teacher by id
         - PUT(id): update a teacher by id
         - DELETE(id): delete a teacher by id
+        - GET (subject): search teacher by subject
     '''
     serializer_class = TeacherViewSerializer
     serializerCreation = TeacherCreationSerializer
     serializerUpdate = TeacherCreationSerializer
+    
+    @action(detail=False, methods=['get'])
+    def search_subject(self, request):
+        '''
+        Search teacher by subject
+        '''
+        subject = request.query_params.get('subject')
+        if subject:
+            queryset = self.get_queryset().filter(subjects__name__icontains=subject)
+            serializer = self.get_serializer(queryset, many=True)
+            if serializer.data:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'detail': 'No teacher found'}, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class ScheduleViewSet(GenericModelViewSet):
     '''
