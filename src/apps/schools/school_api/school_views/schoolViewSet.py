263c39fe0e50b8de0e02a88d856ec5ab42c3bdc0
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-07 11:32:50 -0500

    Search school by name feature done

--- src/apps/schools/school_api/school_views/schoolViewSet.py
+++ src/apps/schools/school_api/school_views/schoolViewSet.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from apps.shared.shared_api.shared_views.GenericModelViewSets import GenericModelViewSet
 from apps.schools.school_api.school_serializers.schoolSerializers import SchoolSerializer, SchoolManagerSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 class SchoolViewset(GenericModelViewSet):
     '''
@@ -10,8 +13,22 @@ class SchoolViewset(GenericModelViewSet):
         - GET(id): get a school by id
         - PUT(id): update a school by id
         - DELETE(id): delete a school by id
+        - GET (name): search school by name
     '''
     serializer_class = SchoolSerializer
+    
+    @action(detail=False, methods=['get'])
+    def search_name(self, request):
+        '''
+        Search school by name
+        '''
+        name = request.query_params.get('name')
+        if name:
+            queryset = self.get_queryset().filter(name__icontains=name)
+            serializer = self.get_serializer(queryset, many=True)
+            if serializer.data:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'detail': 'No school found'}, status=status.HTTP_400_BAD_REQUEST)
 
 class SchoolManagerViewset(GenericModelViewSet):
     '''
