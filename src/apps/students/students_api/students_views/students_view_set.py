df8be2ef7bf22b0d1847d631df722c4af19ed0ec
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-08 15:34:59 -0500

    Filter student by identification number and name feature done

--- src/apps/students/students_api/students_views/students_view_set.py
+++ src/apps/students/students_api/students_views/students_view_set.py
@@ -8,7 +8,10 @@ from apps.students.students_api.students_serializers.relatives_serializers impor
     RelativeViewSerializer,
     RelativeCreationSerializer,
 )
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
 
 class StudentViewSet(GenericModelViewSet):
     """
@@ -18,13 +21,41 @@ class StudentViewSet(GenericModelViewSet):
         - GET(id): get a student by id
         - PUT(id): update a student by id
         - DELETE(id): delete a student by id
+        - search_identification_number(id_number): search a student by id_number
+        - search_first_name(name): search a student by first name
     """
 
     serializer_class = StudentViewSerializer
     serializerCreation = StudentCreationSerializer
     serializerUpdate = StudentCreationSerializer
+    
+    @action(detail=False, methods=['get'])
+    def search_identification_number(self, request):
+        """
+        Search a student by identification_number
+        """
+        identification_number = request.query_params.get('identification_number')
+        if identification_number:
+            queryset = self.get_queryset().filter(identification_number__icontains=identification_number)
+            serializer = self.get_serializer(queryset, many=True)
+            if serializer.data:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'detail': 'No student found'}, status=status.HTTP_400_BAD_REQUEST)
 
-
+    @action(detail=False, methods=['get'])
+    def search_name(self, request):
+        """
+        Search a student by name (first_name or last_name)
+        """
+        name = request.query_params.get('name')
+        if name:
+            queryset = self.get_queryset().filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
+            serializer = self.get_serializer(queryset, many=True)
+            if serializer.data:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'detail': 'No student found'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
 class RelativeViewSet(GenericModelViewSet):
     """
     Generic ViewSet for Relative Model
