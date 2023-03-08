90b70449c005dfbe40b4f86d3cf624979d8088cc
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-03-08 13:00:02 -0500

    User viewset created, failed at user creation

--- src/apps/users/user_api/user_routers.py
+++ src/apps/users/user_api/user_routers.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from apps.users.user_api.user_views.userViewSets import UserViewSet
 
 router = DefaultRouter()
+
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
 ]
--- src/apps/users/user_api/user_views/userViewSets.py
+++ src/apps/users/user_api/user_views/userViewSets.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 
 # import generic view set
 from apps.shared.shared_api.shared_views.GenericModelViewSets import GenericModelViewSet
+from apps.users.user_api.user_serializers.userSerializers import UserViewSerializer, UserCreationSerializer
+
+class UserViewSet(GenericModelViewSet):
+    """
+    Generic viewset for user model
+        - GET: list all users
+        - POST: create a user
+        - GET(id): get a user by id
+        - PUT(id): update a user by id
+        - DELETE(id): delete a user by id
+    """
+    serializer_class = UserViewSerializer
+    serializerCreation = UserCreationSerializer
+    serializerUpdate = UserCreationSerializer
