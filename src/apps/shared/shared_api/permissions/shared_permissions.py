f6945dbc4f1a643524f7d27091900733bcbf6405
Alejandro Morales <a.moralesc@javeriana.edu.co>
2023-03-28 10:30:52 -0500

    Update permission handling in GenericModelViewSet

--- src/apps/shared/shared_api/permissions/shared_permissions.py
+++ src/apps/shared/shared_api/permissions/shared_permissions.py
@@ -1,7 +1,26 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission as RestBasePermission
 from rest_framework.request import Request
 
 from apps.users.models import User
+
+
+class BasePermission(RestBasePermission):
+    """
+    Base permission class for all permissions.
+    By default, if the request has an user that is authenticated and is a superuser,
+    the permission is granted.
+    """
+
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user and request.user.is_authenticated and request.user.is_superuser
+        )
+
+
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to authenticated superusers.
+    """
 
 
 class IsAuthenticated(BasePermission):
@@ -9,7 +28,7 @@ class IsAuthenticated(BasePermission):
     Allows access only to authenticated users.
     """
 
-    def has_permission(self, request: Request, _view: object) -> bool:
+    def has_permission(self, request: Request, view: any) -> bool:
         return request.user and request.user.is_authenticated
 
 
@@ -18,44 +37,96 @@ class IsStaffUser(BasePermission):
     Allows access only to staff users.
     """
 
-    def has_permission(self, request: Request, _view: object) -> bool:
-        return request.user and request.user.is_staff
-
-
-class IsSuperUser(BasePermission):
-    """
-    Allows access only to superusers.
-    """
-
-    def has_permission(self, request: Request, _view: object) -> bool:
-        return request.user and request.user.is_superuser
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and request.user.is_staff
+            or super().has_permission(request, view)
+        )
 
 
 class IsAdminRole(BasePermission):
     """
-    Allows access only to users with User.ADMIN role.
+    Allows access only to users with the User.ADMIN role.
     """
 
-    def has_permission(self, request: Request, _view: object) -> bool:
-        return request.user and request.user.role == User.ADMIN
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and request.user.role == User.ADMIN
+            or super().has_permission(request, view)
+        )
 
 
 class IsTeacherRole(BasePermission):
     """
-    Allows access only to users with User.TEACHER or User.ADMIN role.
+    Allows access only to users with the User.TEACHER role.
     """
 
-    def has_permission(self, request: Request, _view: object) -> bool:
-        return request.user and request.user.role in (User.TEACHER, User.ADMIN)
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and request.user.role == User.TEACHER
+            or super().has_permission(request, view)
+        )
 
 
 class IsSchoolManagerRole(BasePermission):
     """
-    Allows access only to users with User.SCHOOL_MANAGER or User.ADMIN role.
+    Allows access only to users with the User.SCHOOL_MANAGER role.
     """
 
-    def has_permission(self, request: Request, _view: object) -> bool:
-        return request.user and request.user.role in (User.SCHOOL_MANAGER, User.ADMIN)
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and request.user.role == User.SCHOOL_MANAGER
+            or super().has_permission(request, view)
+        )
+
+
+class IsAdminOrTeacherRole(BasePermission):
+    """
+    Allows access only to users with the User.ADMIN or User.TEACHER role.
+    """
+
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and (request.user.role == User.ADMIN or request.user.role == User.TEACHER)
+            or super().has_permission(request, view)
+        )
+
+
+class IsAdminOrSchoolManagerRole(BasePermission):
+    """
+    Allows access only to users with the User.ADMIN or User.SCHOOL_MANAGER role.
+    """
+
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and (
+                request.user.role == User.ADMIN
+                or request.user.role == User.SCHOOL_MANAGER
+            )
+            or super().has_permission(request, view)
+        )
+
+
+class IsTeacherOrSchoolManagerRole(BasePermission):
+    """
+    Allows access only to users with the User.TEACHER or User.SCHOOL_MANAGER role.
+    """
+
+    def has_permission(self, request: Request, view: any) -> bool:
+        return (
+            request.user
+            and (
+                request.user.role == User.TEACHER
+                or request.user.role == User.SCHOOL_MANAGER
+            )
+            or super().has_permission(request, view)
+        )
 
 
 class IsSameUser(BasePermission):
@@ -63,9 +134,9 @@ class IsSameUser(BasePermission):
     Allows access to resource only if user is the same as the one in the URL.
     """
 
-    def has_permission(self, request: Request, _view: object) -> bool:
+    def has_permission(self, request: Request, view: any) -> bool:
         return (
             request.user
             and request.user.id == request.parser_context["kwargs"]["pk"]
-            or request.user.role == User.ADMIN
+            or super().has_permission(request, view)
         )
--- src/apps/shared/shared_api/shared_views/GenericModelViewSets.py
+++ src/apps/shared/shared_api/shared_views/GenericModelViewSets.py
@@ -1,26 +1,83 @@
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-
-# import serializer
 from rest_framework.serializers import Serializer
 
-# CRUD WITH GENERAL VIEWSET
+from apps.shared.shared_api.permissions.shared_permissions import BasePermission
+
+
 class GenericModelViewSet(ModelViewSet):
     serializer_class = None
     serializer_create_class = None
     serializer_update_class = None
 
-    list_permissions = []
-    create_permissions = []
-    retrieve_permissions = []
-    update_permissions = []
-    destroy_permissions = []
+    list_permission_classes = []
+    retrieve_permission_classes = []
+    create_permission_classes = []
+    update_permission_classes = []
+    destroy_permission_classes = []
 
     def get_queryset(self):
         model = self.get_serializer().Meta.model
         return model.objects.filter(is_active=True)
+
+    def get_serializer_class(self) -> Serializer:
+        if self.action == "create":
+            assert self.serializer_class is not None, (
+                "'%s' should either include a `serializer_create_class` attribute, "
+                "or override the `get_serializer_class()` method."
+                % self.__class__.__name__
+            )
+            return self.serializer_create_class
+        elif self.action == "update":
+            assert self.serializer_class is not None, (
+                "'%s' should either include a `serializer_update_class` attribute, "
+                "or override the `get_serializer_class()` method."
+                % self.__class__.__name__
+            )
+            return self.serializer_update_class
+
+        return super().get_serializer_class()
+
+    def get_permissions(self) -> list[BasePermission]:
+        if self.action == "list":
+            return [
+                permission()
+                for permission in (
+                    self.list_permission_classes + self.permission_classes
+                )
+            ]
+        elif self.action == "retrieve":
+            return [
+                permission()
+                for permission in (
+                    self.retrieve_permission_classes + self.permission_classes
+                )
+            ]
+        elif self.action == "create":
+            return [
+                permission()
+                for permission in (
+                    self.create_permission_classes + self.permission_classes
+                )
+            ]
+        elif self.action == "update":
+            return [
+                permission()
+                for permission in (
+                    self.update_permission_classes + self.permission_classes
+                )
+            ]
+        elif self.action == "destroy":
+            return [
+                permission()
+                for permission in (
+                    self.destroy_permission_classes + self.permission_classes
+                )
+            ]
+
+        return super().get_permissions()
 
     def list(self, request: Request, *args, **kwargs) -> Response:
         """Get all objects of the model.
@@ -35,6 +92,23 @@ class GenericModelViewSet(ModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request: Request, *args, **kwargs) -> Response:
+        """Get an object of the model by id.
+
+        Args:
+            request (Request): HTTP request information with the id of the object
+
+        Returns:
+            Response: HTTP response with the object or 404 if the object is not found
+        """
+
+        instance = self.get_object()
+        if instance:
+            return Response(
+                self.get_serializer(instance).data, status=status.HTTP_200_OK
+            )
+        return Response({"error": "Object not found"}, status=status.HTTP_404_NOT_FOUND)
 
     def create(self, request: Request, *args, **kwargs) -> Response:
         """Create an object of the model.
@@ -53,23 +127,6 @@ class GenericModelViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def retrieve(self, request: Request, *args, **kwargs) -> Response:
-        """Get an object of the model by id.
-
-        Args:
-            request (Request): HTTP request information with the id of the object
-
-        Returns:
-            Response: HTTP response with the object or 404 if the object is not found
-        """
-
-        instance = self.get_object()
-        if instance:
-            return Response(
-                self.get_serializer(instance).data, status=status.HTTP_200_OK
-            )
-        return Response({"error": "Object not found"}, status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request: Request, *args, **kwargs) -> Response:
         """Update an object of the model.
@@ -111,18 +168,3 @@ class GenericModelViewSet(ModelViewSet):
                 {"message": "Object deleted successfully"}, status=status.HTTP_200_OK
             )
         return Response({"error": "Object not found"}, status=status.HTTP_404_NOT_FOUND)
-
-    def get_serializer(self, *args, **kwargs) -> Serializer:
-        # Create serializer
-        if self.action == "create":
-            if self.serializer_create_class:
-                kwargs.setdefault("context", self.get_serializer_context())
-                return self.serializer_create_class(*args, **kwargs)
-
-        # Update serializer
-        elif self.action == "update":
-            if self.serializer_update_class:
-                kwargs.setdefault("context", self.get_serializer_context())
-                return self.serializer_update_class(*args, **kwargs)
-
-        return super().get_serializer(*args, **kwargs)
